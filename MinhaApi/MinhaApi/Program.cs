002f95f831b2b2cfad80355de535a1ef7228389b
Miller Vasco <millermartinsvasco1@gmail.com>
2022-04-20 16:18:01 -0300

    161720042022

--- MinhaApi/MinhaApi/Program.cs
+++ MinhaApi/MinhaApi/Program.cs
@@ -1,6 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () =>
+{
+    return Results.Ok("Hello World!");
+});
+
+app.MapGet("/{nome}", (string nome) =>
+{
+    return Results.Ok($"Hello {nome}!");
+});
+
+app.MapPost("/", (User user) =>
+{
+    return Results.Ok(user);
+});
+
 
 app.Run();
+
+public class User
+{
+    public int Id { get; set; }
+    public string UserName { get; set; }
+}
