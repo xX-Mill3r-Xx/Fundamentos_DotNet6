918552486de39eb453139637d6441173a494facb
Miller Vasco <millermartinsvasco1@gmail.com>
2022-04-21 20:54:00 -0300

    205321042022

--- Blog/Blog/Program.cs
+++ Blog/Blog/Program.cs
@@ -1,6 +1,27 @@
+using System.Text;
+using Blog;
 using Blog.Data;
+using Blog.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
 
 builder
     .Services
@@ -10,9 +31,13 @@ builder
         options.SuppressModelStateInvalidFilter = true;
     });
 builder.Services.AddDbContext<BlogDataContext>();
-builder.Services.AddMvc();
+builder.Services.AddTransient<TokenService>();
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
