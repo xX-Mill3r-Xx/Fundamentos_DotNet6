ad954338d4855929721c2e0e32bba97d1e04fd67
Miller Vasco <millermartinsvasco1@gmail.com>
2022-04-22 08:47:34 -0300

    084722042022

--- Blog/Blog/Configuration.cs
+++ Blog/Blog/Configuration.cs
@@ -1,9 +1,17 @@
-﻿namespace Blog
+﻿namespace Blog;
+
+public static class Configuration
 {
-    public static class Configuration
+    public static string JwtKey = "ZmVkYWY3ZDg4NjNiNDhlMTk3YjkyODdkNDkyYjcwOGU=";
+    public static string ApiKeyName = "api_key";
+    public static string ApiKey = "curso_api_IlTevUM/z0ey3NwCV/unWg==";
+    public static SmtpConfiguration Smtp = new();
+
+    public class SmtpConfiguration
     {
-        public static string JwtKey = "ZmVkYWY3ZDg4NjNiNDhlMTk3YjkyODdkNDkyYjcwOGU=";
-        public static string ApiKeyName = "api_key";
-        public static string ApiKey = "curso_api_IlTevUM/z0ey3NwCV/unWg==";
+        public string Host { get; set; }
+        public int Port { get; set; } = 25;
+        public string UserName { get; set; }
+        public string Password { get; set; }
     }
 }
